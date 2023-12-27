d3bc61d1f6641e835a7cbab6ddefc0f54c3709f5
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-12-27 23:19:25 +0530

    added redux store and canvas API

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,4 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Toolbox from "../ToolBoard/Toolbox";
 import {
   faPencil,
   faEraser,
@@ -8,21 +7,42 @@ import {
   faDownload,
 } from "@fortawesome/free-solid-svg-icons";
 const Menu = () => {
+  const handlePencilClick = () => {};
+  const handleEraserClick = () => {};
+  const handleUndoClick = () => {};
+  const handleRedoClick = () => {};
+  const handleDownload = () => {};
+
   return (
     <div className="absolute px-6 py-2 flex justify-between w-1/4 left-1/2 top-7 -translate-x-1/2 border border-solid border-zinc-300 rounded-md">
-      <div className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md">
+      <div
+        className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
+        onClick={handlePencilClick}
+      >
         <FontAwesomeIcon icon={faPencil} />
       </div>
-      <div className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md">
+      <div
+        className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
+        onClick={handleEraserClick}
+      >
         <FontAwesomeIcon icon={faEraser} />
       </div>
-      <div className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md">
+      <div
+        className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
+        onClick={handleUndoClick}
+      >
         <FontAwesomeIcon icon={faRotateLeft} />
       </div>
-      <div className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md">
+      <div
+        className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
+        onClick={handleRedoClick}
+      >
         <FontAwesomeIcon icon={faRotateRight} />
       </div>
-      <div className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md">
+      <div
+        className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
+        onClick={handleDownload}
+      >
         <FontAwesomeIcon icon={faDownload} />
       </div>
     </div>
---
+++ src/store/slice/menuSlice.jsx
@@ -0,0 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const menuSlice = createSlice({});
+
+export default menuSlice;
