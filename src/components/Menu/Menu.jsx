4ff340770cc09549eccebc7cc7660acd15b63189
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-12-28 02:25:39 +0530

    fix: import errors

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,42 +6,47 @@ import {
   faRotateRight,
   faDownload,
 } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { clickMenuItem, clickActionItem } from "../../store/slice/menuSlice";
+import MENU_ITEMS from "../../utils/constants";
+
 const Menu = () => {
-  const handlePencilClick = () => {};
-  const handleEraserClick = () => {};
-  const handleUndoClick = () => {};
-  const handleRedoClick = () => {};
-  const handleDownload = () => {};
+  const dispatch = useDispatch();
+  const handleMenuClick = (item) => {
+    dispatch(clickMenuItem(item));
+  };
+
+  const handleActionClick = () => {};
 
   return (
     <div className="absolute px-6 py-2 flex justify-between w-1/4 left-1/2 top-7 -translate-x-1/2 border border-solid border-zinc-300 rounded-md">
       <div
         className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
-        onClick={handlePencilClick}
+        onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}
       >
         <FontAwesomeIcon icon={faPencil} />
       </div>
       <div
         className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
-        onClick={handleEraserClick}
+        onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}
       >
         <FontAwesomeIcon icon={faEraser} />
       </div>
       <div
         className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
-        onClick={handleUndoClick}
+        onClick={() => handleActionClick()}
       >
         <FontAwesomeIcon icon={faRotateLeft} />
       </div>
       <div
         className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
-        onClick={handleRedoClick}
+        onClick={() => handleActionClick()}
       >
         <FontAwesomeIcon icon={faRotateRight} />
       </div>
       <div
         className="flex hover:bg-slate-200 active:bg-slate-200 cursor-pointer h-8 w-8 justify-center items-center rounded-md"
-        onClick={handleDownload}
+        onClick={() => handleActionClick()}
       >
         <FontAwesomeIcon icon={faDownload} />
       </div>
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import menuReducer from "./slice/menuSlice";
 
-const store = configureStore({});
+const store = configureStore({
+  reducer: {
+    menu: menuReducer,
+  },
+});
 
 export default store;
